cd04630782cd07c9964e0a2f763913dc1c360fe0
HugoCapocci <h.capocci@gmail.com>
2019-05-02 22:53:44 +0200

    Implement logs reducer

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,28 @@
-    
-import { createStore } from 'redux';
+ import { createStore } from 'redux';
 
-const defaultState = {
+import { LogsActionTypes, ADD_LINE, CLEAR_LINES } from './types/logs';
+
+export interface StateProps {
+  lines: Array<string>
+}
+
+const defaultState: StateProps = {
   lines: []
 };
 
-function reducer(state = defaultState, action: any) {
+function reducer(state = defaultState, action: LogsActionTypes): StateProps {
   switch (action.type) {
+    case ADD_LINE:
+      return {
+        lines: [
+          ...state.lines,
+          action.payload
+        ]
+      };
+    case CLEAR_LINES:
+      return {
+        lines: []
+      };
     default:
       return state;
   }
