59991b343d97de78b2dd6679fb75e288b00501e7
Hugo CAPOCCI <h.capocci@gmail.com>
2019-05-02 07:45:09 +0200

    Add 'logs' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Logs from './components/Logs';
 import './App.css';
 
 export default class App extends Component {
@@ -7,19 +7,8 @@ export default class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Logs lines={[]} />
       </div>
     );
   }
