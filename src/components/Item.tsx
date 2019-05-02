18e596652a094285831b9d24a77c5a0c2cdc36ba
HugoCapocci <h.capocci@gmail.com>
2019-05-02 22:42:35 +0200

    Connect add line action

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import { Dispatch} from 'redux';
+import { connect } from 'react-redux';
+
+import { addLine } from '../actions/logs';
+import { LogsActionTypes } from '../types/logs';
 
 export interface ItemProps {
     label: string,
     id: number
 }
 
-export default class Item extends Component<ItemProps> {
+interface DispatchProps {
+  addLine: () => void;
+}
+
+type Props = ItemProps & DispatchProps;
+
+export class Item extends Component<Props> {
   static defaultProps = {
     label: 'ITEM'
   };
   render() {
-    // TODO add message `render Item Component with id ${this.props.id}` to logs
+    this.props.addLine();
     return <div>{this.props.label} { this.props.id }</div>
   }
 }
+
+const mapDispatchToProps = (dispatch: Dispatch<LogsActionTypes>, ownProps: ItemProps): DispatchProps => ({
+  addLine: () => {
+    dispatch(addLine(`render Item Component with id ${ownProps.id}`))
+  }
+});
+
+export default connect<{}, DispatchProps, ItemProps>(
+  null,
+  mapDispatchToProps
+)(Item);
