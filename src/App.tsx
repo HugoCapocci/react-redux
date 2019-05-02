951b6076b6160d0469472a86c91930ef1b74ed2c
hcapocci <hcapocci@talentsoft.com>
2019-05-02 16:22:31 +0200

    add Item & Item box components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import Logs from './components/Logs';
 import './App.css';
 
+import Logs from './components/Logs';
+import ItemBox from './components/ItemBox';
+
 export default class App extends Component {
+  state = {
+    itemBoxSize: 3,
+    itemsLabel: 'ITEM',
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         </header>
         <Logs lines={[]} />
+      <div className="container">
+      <div className="components">
+        <label htmlFor="itemBoxSize">Nb Components:</label>
+        <input
+          type="number"
+          name="itemBoxSize"
+          id="itemBoxSize"
+          value={this.state.itemBoxSize}
+          />
+        <label htmlFor="itemsLabel">Label:</label>
+        <input
+          type="text"
+          name="itemsLabel"
+          id="itemsLabel"
+          value={this.state.itemsLabel}
+          />
+        <ItemBox size={this.state.itemBoxSize} itemsLabel={this.state.itemsLabel} />
       </div>
+      </div>
+    </div>
     );
   }
 }
